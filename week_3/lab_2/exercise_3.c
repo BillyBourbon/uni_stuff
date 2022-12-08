796c0c9a2122a3ecb42f6dbc381d1001de41f6af
BillyBourbon <87441988+BillyBourbon@users.noreply.github.com>
2022-12-08 15:56:36 +0000

    Create exercise_3.c

---
+++ week_3/lab_2/exercise_3.c
@@ -0,0 +1,19 @@
+// Exercise 3: Given three values a ≤ b ≤ c, which are the lengths of three segments, test if
+// they can be the sides of a triangle using conditional statements. Insert the values from
+// keyboard and print "It's a triangle" or "It's not a triangle". If so, dene the type of
+// triangle: equilateral, isosceles, or scalene. Use if...else statments.
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  int x, y, z;
+  printf("Enter The First Length\n"); scanf("%i",&x);
+  printf("Enter The Second Length\n"); scanf("%i",&y);
+  printf("Enter The Third Length\n"); scanf("%i",&z);
+
+  if( x == y == z) printf("Your Triangle Is Equalateral With Lengths Size %i",x);
+  if( x == y || y == z || x == z) printf("Your Triangle Is Isocele With Sides Length %i, %i, %i",x,y,z);
+  if( x != y != z) printf("Your Triangle Is Scalene With Side Lengths %i, %i, ");
+  return 0;
+}
